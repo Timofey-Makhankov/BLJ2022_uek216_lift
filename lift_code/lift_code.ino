b6f18463c113f677c07f83a8480c45a756cd6f9f
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2022-11-23 11:49:08 +0100

    added abiliy to use Display

--- lift_code/lift_code.ino
+++ lift_code/lift_code.ino
@@ -1,20 +1,34 @@
 #include <Servo.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
 #define LED 15
 #define LIGHTSENSORPIN 13
 #define BUTTON 14
 
 Servo servo;
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 
 int pos;
 
 void setup() {
   Serial.begin(115200);
+
   pinMode(LED, OUTPUT);
   pinMode(LIGHTSENSORPIN, INPUT);
   pinMode(BUTTON, INPUT);
+
   servo.attach(12);
   servo.write(90);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("SSD1306 allocation failed");
+    while (1) delay(1);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 }
 void loop() {
   float reading = analogRead(LIGHTSENSORPIN);
@@ -36,4 +50,12 @@ void loop() {
     delay(400);
     servo.write(90);
   }
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(reading);
+  display.print(" LuX");
+
+  display.display();
+  delay(100);
 }
