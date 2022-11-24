3adf4f8ffd76da70021e21c9f8b0909b666ddd76
Loris Di Leo <loris.dileo@noseryoung.com>
2022-11-24 14:18:29 +0100

    added a Slave node Code

---
+++ lift_node_2/lift_node_2.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define SERVO_PIN 15
+#define LIGHT_PIN 33
+
+#define SPIN_TIME 400
+
+char mac[17];
+const char* ssid = "GuestWLANPortal";
+const char* mqtt_server = "142.93.174.193";
+
+const char* topic1 = "zuerich/lift/lightsensor2";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+Servo servo;
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+
+  pinMode(LIGHT_PIN, INPUT);
+}
+
+void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("done!");
+  byte m[6];
+  WiFi.macAddress(m);
+  sprintf(mac, "%02X:02X:02X:02X:02X:02X", m[0], m[1], m[2], m[3], m[4], m[5]);
+}
+
+void reconnect() {
+  Serial.print("Attempting MQTT connection...");
+  while (!client.connected()) {
+    if (client.connect("lift")) {
+      Serial.println("done!");
+      client.subscribe(topic1);
+    } else {
+      delay(500);
+      Serial.print(".");
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) { reconnect(); }
+  char buffer[15];
+  float reading = analogRead(LIGHT_PIN);
+  sprintf(buffer, "%f", reading);
+  client.loop();
+  client.publish(topic1, buffer);
+  client.loop();
+  delay(500);
+}
+
